371d54cce5af2954a3bb9a2398109d59a8d9a5b1
Raghu Betina <raghu@firstdraft.com>
2018-03-02 15:54:32 +0000

    Direct association between teachers and plan_comments

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,10 @@
 class Teacher < ApplicationRecord
   # Direct associations
 
+  has_many   :plan_comments,
+             :foreign_key => "user_id",
+             :dependent => :destroy
+
   has_many   :improvements,
              :class_name => "ImprovementPlan",
              :dependent => :destroy
