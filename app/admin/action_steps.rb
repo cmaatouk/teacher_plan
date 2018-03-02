f99faf5eef34fd29168c4d2adf170dfaeed9b3d8
Raghu Betina <raghu@firstdraft.com>
2018-03-02 15:54:54 +0000

    Whitelist columns in ActionStep for ActiveAdmin

--- app/admin/action_steps.rb
+++ app/admin/action_steps.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register ActionStep do
+
+ permit_params :descriptions, :target_dates, :leader, :necessary_resources, :implementation_specifics, :measure_of_success, :goal_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
